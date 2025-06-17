5f6ad8a3e5d961f60c0f3f9eff69eca1a7b5fab6
Anfa Majid <110888247+anfa-majid@users.noreply.github.com>
2025-06-17 17:29:57 +0500

    removed share_plus

--- ghalib/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ghalib/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,9 +12,7 @@ import firebase_core
 import flutter_local_notifications
 import google_sign_in_ios
 import path_provider_foundation
-import share_plus
 import shared_preferences_foundation
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -24,7 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
